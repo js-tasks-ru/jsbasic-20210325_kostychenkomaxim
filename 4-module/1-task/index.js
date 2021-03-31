5636cc459ffdd1a02c221a4ecfead42789480afe
Kostiuchenko Maksym <maksym.kostiuchenko@masterofcode.com>
2021-03-31 23:43:20 +0300

    make friends list

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const usersList = document.createElement('ul');
+  friends.map(user => {
+    const userItem = document.createElement('li');
+    userItem.innerHTML = `${user.firstName} ${user.lastName}`;
+    usersList.appendChild(userItem);
+  });
+  return usersList;
 }
