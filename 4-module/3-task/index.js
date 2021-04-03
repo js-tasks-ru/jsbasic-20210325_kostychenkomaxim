42ffe3bb2c23245414c255fb3a305c48bc12ae19
Kostiuchenko Maksym <maksym.kostiuchenko@masterofcode.com>
2021-04-03 22:11:42 +0300

    task with table

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
   // ваш код...
+  const rows = table.getElementsByTagName("tr");
+  Array.from(rows).forEach(
+    (row) => {
+      const statusTd = row.getElementsByTagName('td')[3];
+      const genderTd = row.getElementsByTagName('td')[2];            
+      const ageTd = row.getElementsByTagName('td')[1];
+      if (!statusTd.dataset.available) {
+        row.hidden = true;
+      }
+      else if (statusTd.dataset.available === "true") {
+        row.classList.add('available');
+      } 
+      else if (statusTd.dataset.available === "false") {
+        row.classList.add('unavailable');
+      } 
+      
+      if (genderTd.textContent === 'm') {
+        row.classList.add('male');
+      }
+      else if (genderTd.textContent === 'f') {
+        row.classList.add('female');
+      }
+
+      if (Number(ageTd.textContent) < 18) {
+        row.style.textDecoration = 'line-through';
+      }
+    }
+  );
 }
