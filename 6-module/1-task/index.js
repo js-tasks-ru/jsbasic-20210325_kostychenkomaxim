a1f08578e5f2cfe69c8d47a74303769a8967e677
Kostiuchenko Maksym <maksym.kostiuchenko@masterofcode.com>
2021-04-14 16:23:27 +0300

    remove tr from table on button click

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,37 @@
  */
 export default class UserTable {
   constructor(rows) {
-  }
+    this.elem = document.createElement('table');
+    this.elem.insertAdjacentHTML('afterbegin', tableCaption);
+    this.tableBody = document.createElement('tbody');
+    this.elem.appendChild(this.tableBody);
+
+    rows.map(elem => {
+      const row = document.createElement("tr");
+    
+      row.innerHTML = tableTd(elem);
+      this.tableBody.appendChild(row);
+
+      const buttonRef = row.querySelector('button');
+
+      buttonRef.addEventListener('click', () => {
+        buttonRef.closest('tr').remove();
+      });
+
+    });
+  } 
+
 }
+
+const tableCaption =
+  `<thead>
+  <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+  </tr>
+`;
+
+const tableTd = user => `<td>${user.name}</td><td>${user.age}</td><td>${user.salary}</td><td>${user.city}</td><td><button>X</button></td>`;
