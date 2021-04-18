a3d3a07027e641f4c054d5c55943381da2200807
Kostiuchenko Maksym <maksym.kostiuchenko@masterofcode.com>
2021-04-18 10:26:22 +0300

    module #6

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,5 +2,24 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
+    this.elem = createElement(`<div class="card">
+    <div class="card__top">
+        <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+        <span class="card__price">€${product.price.toFixed(2)}</span>
+    </div>
+    <div class="card__body">
+        <div class="card__title">${product.name}</div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>`);
+
+    
+    const buttonRef = this.elem .querySelector('button');
+    
+    buttonRef.addEventListener('click', () => {
+      this.elem.dispatchEvent(new CustomEvent("product-add", { 
+        detail: product.id, bubbles: true }));
+    });
   }
 }
